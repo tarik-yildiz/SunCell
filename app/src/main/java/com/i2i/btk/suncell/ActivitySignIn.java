f1999be7b28c595c9e6bd0338927558088bb178f
Tarık Yıldız <112180001+tarik-yildiz@users.noreply.github.com>
2023-02-16 10:38:23 +0300

    Sign in page, app icon added

---
+++ app/src/main/java/com/i2i/btk/suncell/ActivitySignIn.java
@@ -0,0 +1,63 @@
+package com.i2i.btk.suncell;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class ActivitySignIn extends AppCompatActivity {
+    EditText editTextPhoneNumber, editTextPassword;
+    TextView textViewRegister;
+    ConstraintLayout constraintLayoutLogin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+        initComponents();
+        setOnClicks();
+        fullScreen();
+    }
+    private void initComponents() {
+        editTextPassword=findViewById(R.id.editTextPasswordLogin);
+        editTextPhoneNumber=findViewById(R.id.editTextPhoneNumberLogin);
+        textViewRegister=findViewById(R.id.textViewRegister);
+        constraintLayoutLogin=findViewById(R.id.constraintLayoutSignIn);
+    }
+    private void setOnClicks(){
+        constraintLayoutLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String msg=editTextPassword.getText().toString()+editTextPhoneNumber.getText().toString();
+                //   login();
+                //    Toast.makeText(LoginScreen.this,"Login success"+ msg, Toast.LENGTH_SHORT).show();
+              //  loginSuccess();
+            }
+        });
+        textViewRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(ActivitySignIn.this,ActivitySignUp .class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+    private void fullScreen(){
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+    }
+    private void loginSuccess(){
+    //    Intent intent=new Intent(ActivitySignIn .this,UserInfoActivity.class);
+    //    startActivity(intent);
+    }
+
+}
